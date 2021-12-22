0e20db91d1b1264561fd15016e762d1d6f2c2722
Yining Lu <85244200+yining610@users.noreply.github.com>
2021-12-22 23:53:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -15,22 +15,19 @@ def epsilon_by_frame(request):
         -1. * request / epsilon_decay)
 
 def win_prob_second_list(bi,wi,zi):
-    #print(data)
     bk = list(set(bi))
     dj = list()
     nj = list()
     wo = list()
     for i in bk:
         # loss = len(data[(data["zi"] < i-1) & (data["wi"]==1)]) + len(data[(data["bi"] <= i-1) & (data["wi"]==0)])
-        nj.append(sum([(z >= i-1) & (w == 1) for z,w in zip(zi, wi)]) + sum([(b >= i) & (w == 0) for b,w in zip(bi, wi)]))
-        dj.append(sum([(z == i-1) & (z > 0) for z in zi]))
+        nj.append(np.sum(np.logical_and((np.array(zi) >= (i-1)),np.array(wi) == 1)) + np.sum(np.logical_and((np.array(bi) >= i),np.array(wi) == 0)))
+        dj.append(np.sum(np.logical_and((np.array(zi) == (i-1)),np.array(zi) > 0)))
         # nj.append(8 - loss)
     wo =[1]
     for c,i,j,k in zip(range(len(bk)),bk,nj,dj):
         if c>0:
-             wo.append(((j-k)/(j+0.000001)) * wo[c-1])
-        else:
-            continue
+             wo.append(((j-k)/(j+0.0000001)) * wo[c-1])
     wo = list(map(lambda x:1-x,wo))
     return bk,wo
 
